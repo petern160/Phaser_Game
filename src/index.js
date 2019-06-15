072dfe03c41cce2223da8aaf5e57b946faef27b5
petern160 <peternguyen160@gmail.com>
2019-06-14 22:20:07 -0700

    added platforms to stage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 import skyImg from './assets/sky.png';
+import starImg from './assets/star.png';
+import groundImg from './assets/platform.png';
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug:false
+    }
+  },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
@@ -19,8 +29,8 @@ const game = new Phaser.Game(config);
 // loads assest we need with var name then location
 function preload() {
   this.load.image('sky', skyImg);
-  // this.load.image('ground', 'assets/platform.png');
-  // this.load.image('star', 'assets/star.png');
+  this.load.image('ground', groundImg);
+  // this.load.image('star', starImg);
   // this.load.image('bomb', 'assets/bomb.png');
   // this.load.spritesheet('dude', 
   //     'assets/dude.png',
@@ -30,9 +40,22 @@ function preload() {
 
 // order matters for the spirtes need to put background first then elements after
 function create() {
+  // new image game object adding it to current scenese display list
   this.add.image(400, 300, 'sky');
+  // this.add.image(400, 300, 'star');
+  let platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  //lower right
+  platforms.create(600, 400, 'ground');
+  // middle left
+  platforms.create(50, 250, 'ground');
+  // highest right
+  platforms.create(750, 220, 'ground');
 }
 
+function update ()
+    {
+    }
 
 // this.load.image("logo", logoImg);
 
