d92cce6693dff4ca411dcd0a5cd5db7e0d7cd3d8
petern160 <peternguyen160@gmail.com>
2019-06-14 21:44:42 -0700

    phaser boiler plate setup with loaded assets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
+import skyImg from './assets/sky.png';
 
 const config = {
   type: Phaser.AUTO,
@@ -14,19 +15,34 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+
+// loads assest we need with var name then location
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('sky', skyImg);
+  // this.load.image('ground', 'assets/platform.png');
+  // this.load.image('star', 'assets/star.png');
+  // this.load.image('bomb', 'assets/bomb.png');
+  // this.load.spritesheet('dude', 
+  //     'assets/dude.png',
+  //     { frameWidth: 32, frameHeight: 48 }
+  // );
 }
 
+// order matters for the spirtes need to put background first then elements after
 function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  this.add.image(400, 300, 'sky');
 }
+
+
+// this.load.image("logo", logoImg);
+
+// const logo = this.add.image(400, 150, "logo");
+
+// this.tweens.add({
+//   targets: logo,
+//   y: 450,
+//   duration: 2000,
+//   ease: "Power2",
+//   yoyo: true,
+//   loop: -1
+// });
